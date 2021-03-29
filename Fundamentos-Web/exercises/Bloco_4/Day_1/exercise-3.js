085fd9e0f25713f803096f0abe0fce36b589de30
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-03-29 18:59:27 -0300

    Adds exercise-3.js

---
+++ Fundamentos-Web/exercises/Bloco_4/Day_1/exercise-3.js
@@ -0,0 +1,13 @@
+// 3. Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+
+let numero4 = 28
+let numero5 = 1450
+let numero6 = 33
+
+if (numero4 > numero5 && numero4 > numero6) {
+  console.log(numero4);
+} else if (numero5 > numero4 && numero5 > numero6){
+  console.log(numero5);
+} else {
+  console.log(numero6)
+}

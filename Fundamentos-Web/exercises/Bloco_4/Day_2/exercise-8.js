ac24c53bc40b9e5a8acabd405cb4fe7e9e196e1b
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-03-30 00:04:39 -0300

    Add exercise-8.js

---
+++ Fundamentos-Web/exercises/Bloco_4/Day_2/exercise-8.js
@@ -0,0 +1,9 @@
+// 8. Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
+
+let twentyFive = []
+
+for (let i = 1; i <= 25; i += 1){
+  twentyFive.push(i)
+}
+
+console.log(twentyFive)

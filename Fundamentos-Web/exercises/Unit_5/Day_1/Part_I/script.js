d478c6ad0d34440901d8acb4cbba6c72c1c36d89
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-04-05 14:38:34 -0300

    Create script.js and style.css files at the  Part_I folder and move index.html to Part_I folder.

---
+++ Fundamentos-Web/exercises/Unit_5/Day_1/Part_I/script.js
@@ -0,0 +1,9 @@
+var paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+// document.getElementById("page-title").innerText = "Interestelar";
+document.getElementById("second-paragraph").innerText = "O melhor filme que já assisti";
+// document.getElementById("subtitle").innerText = "O que acha desse filme?";
+
+document.getElementsByClassName("title")[0].innerText = "Interestelar"
+
+document.getElementsByTagName("h4")[0].innerText = "O que acha desse filme?"

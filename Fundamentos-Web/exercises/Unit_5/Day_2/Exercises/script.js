f967c21568bd296dd77e3c8459a255346a5f9ad1
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-04-06 18:34:16 -0300

    Add Exercises folder, index.html and script.js files and ends  exercises 1

---
+++ Fundamentos-Web/exercises/Unit_5/Day_2/Exercises/script.js
@@ -0,0 +1,8 @@
+// O objetivo desses exercícios é colocar em prática o que você acabou de aprender sobre DOM. Por isso, você deve fazer os exercícios utilizando apenas código JavaScript , o qual deve ser inserido entre as tags <script> e </script> .
+// Para uma melhor organização, faça commits a cada tarefa concluída. Vamos aos exercícios:
+
+// 1. Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
+
+let tituloH1 = document.createElement('h1');
+tituloH1.innerText = "Exercício 5.2 - JavaScript DOM";
+document.querySelector('body').appendChild(tituloH1);

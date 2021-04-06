7d68b832f0680f607762e8e757a65a2985a770cb
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-04-06 11:08:52 -0300

    Add exercises Part_I

--- Fundamentos-Web/exercises/Unit_5/Day_2/Part_I/script.js
+++ Fundamentos-Web/exercises/Unit_5/Day_2/Part_I/script.js
@@ -7,3 +7,18 @@
 // 7. Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta;
 // 8. Agora acesse o terceiroFilho a partir de pai.
 
+document.querySelector('#elementoOndeVoceEsta');
+
+document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = "red";
+
+document.querySelector('#primeiroFilhoDoFilho').innerHTML = "<p> Adiciona um texto ao elemento que contem o <em>id</em> primeiroFilhoDoFilho.</p>";
+
+document.querySelector('#pai').firstElementChild;
+
+document.querySelector('#elementoOndeVoceEsta').parentNode.firstElementChild;
+
+document.querySelector('#elementoOndeVoceEsta').parentNode.innerText;
+
+document.querySelector('#elementoOndeVoceEsta').parentElement.children[2];
+
+document.querySelector('#pai').children[2];

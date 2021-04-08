7e31ed2e428d39adb9cc47639c8c8df5053fb406
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-04-08 15:45:59 -0300

    Add exercise 1

--- Fundamentos-Web/exercises/Unit_5/Day_3/exercises/script.js
+++ Fundamentos-Web/exercises/Unit_5/Day_3/exercises/script.js
@@ -14,3 +14,30 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+// Exercício 1:
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+for (let index = 0; index < dezDaysList.length; index += 1) {
+  let monthDay = dezDaysList[index];
+
+  let sundayToSaturday = document.getElementById('days');
+  let weekDay = document.createElement('li');
+  
+  weekDay.className = 'day';
+
+  if (dezDaysList[index] === 24 || dezDaysList[index] === 25 || dezDaysList[index] === 31) {
+    weekDay.classList.add('holiday');
+  }
+
+  if (dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18 || dezDaysList[index] === 25) {
+    weekDay.classList.add('friday');
+  }
+
+  weekDay.innerText = monthDay;
+  
+  sundayToSaturday.appendChild(weekDay);
+
+};
+

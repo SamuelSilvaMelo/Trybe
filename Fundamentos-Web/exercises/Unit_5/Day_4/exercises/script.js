680adb003032023225f1a5ad67f3b5047f74e060
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-04-17 15:55:24 -0300

    Add changeTextColor function

--- Fundamentos-Web/exercises/Unit_5/Day_4/exercises/script.js
+++ Fundamentos-Web/exercises/Unit_5/Day_4/exercises/script.js
@@ -1,7 +1,9 @@
-const personalizationButtons = document.querySelector('.personalization-buttons')
-const bodyStyle = document.querySelector('body').style
+const personalizationButtons = document.querySelector('.personalization-buttons');
+const bodyStyle = document.querySelector('body').style;
+const textColor = document.querySelector('.text-color');
+const paragraph = document.querySelectorAll('.paragraph');
 
-
+// Function to change the site backgroun color
 function changeBackgroundColor(event) {
   if (event.target.id === 'background-white') {
     bodyStyle.backgroundColor = 'white';
@@ -18,3 +20,24 @@ function changeBackgroundColor(event) {
 }
 
 personalizationButtons.addEventListener('click', changeBackgroundColor);
+
+//function to change the text color
+function changeTextColor(event) {
+
+  for (let index = 0; index < paragraph.length; index += 1) {
+    if (event.target.id === 'text-white') {
+      paragraph[index].style.color = 'white';
+    }else if (event.target.id === 'text-grey') {
+      paragraph[index].style.color = 'grey';
+    }else if (event.target.id === 'text-dark-blue') {
+      paragraph[index].style.color = 'darkblue';
+    }else if (event.target.id === 'text-pink') {
+      paragraph[index].style.color = 'pink';
+    } else if (event.target.id === 'text-brown') {
+      paragraph[index].style.color = 'brown';
+    }
+  }
+  
+}
+
+textColor.addEventListener('click', changeTextColor);

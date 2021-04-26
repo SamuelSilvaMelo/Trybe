de59262190dd6495e66a4c2cf3f4397a04d36f15
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-04-26 18:39:57 -0300

    Add exercise 2 - Part-I

--- Fundamentos-Web/exercises/Unit_7/Part-I/script.js
+++ Fundamentos-Web/exercises/Unit_7/Part-I/script.js
@@ -15,3 +15,17 @@ const testingScope = escopo => {
 }
 
 testingScope(true);
+
+// 2. Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(oddsAndEvens); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+// Copie o código abaixo.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortArray = array => array.sort((a, b) => a - b);
+
+console.log(`Os números: ${sortArray(oddsAndEvens)} se encontram ordenados de forma crescente!`);
+
+// Referência para fazer esse exercício:
+// 1. https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort

33032210092917c044bba7622e290d481dd7fae5
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-06-07 09:21:26 -0300

    Add random-user-data

--- 2.Front-end-Development/exercises/Unit_13/Day_1/class-exercises/random-user-data/src/App.js
+++ 2.Front-end-Development/exercises/Unit_13/Day_1/class-exercises/random-user-data/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.requestRandomUser = this.requestRandomUser.bind(this);
+    this.renderRandomUser = this.renderRandomUser.bind(this);
+
+    this.state = {
+      randomUser: [],
+      isLoading: true,
+    }
+
+    console.log('constructor')
+  }
+
+  async requestRandomUser() {
+    this.setState(
+      {},
+      async () => {
+        await fetch('https://api.randomuser.me/')
+        .then(response => response.json())
+        .then(data => this.setState({ randomUser: data.results }))
+
+        this.setState({
+          isLoading: false,
+        })
+      }
+    )
+  }
+
+  componentDidMount() {
+    this.requestRandomUser();
+  }
+
+  renderRandomUser() {
+    const { randomUser } = this.state;
+
+    return randomUser.map((user) => {
+      return (
+        <div>
+          <h2>Random User</h2>
+          <img src={ user.picture.large } alt={`${ user.name.first }`} />
+          <p>Name: {`${ user.name.title }. ${ user.name.first } ${ user.name.last }`} </p>
+          <p>Email: { user.email } </p>
+          <p>Age: {`${ user.registered.age } years`} </p>
+          <span>Reload to change user.</span>
+        </div>
+      )
+    });
+  }
+
+  loading() {
+    return (
+      <p>Loading...</p>
+    );
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <>
+        { isLoading ? this.loading() : this.renderRandomUser() }
+      </>
+    );
+  }
 }
 
 export default App;

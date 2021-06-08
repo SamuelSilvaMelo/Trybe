56f655f7080f6dd18091efd5f269d16af8694710
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-06-07 22:53:17 -0300

    Add fixation-exercise 6

--- 2.Front-end-Development/exercises/Unit_13/Day_2/class-exercises/comprehension-exercises-router-master/src/App.js
+++ 2.Front-end-Development/exercises/Unit_13/Day_2/class-exercises/comprehension-exercises-router-master/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import './App.css'
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users'
@@ -9,9 +10,20 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ Home } />
-        <Route path="/about" component={ About } />
-        <Route path="/users" component={ Users } />
+        <header>
+          <nav>
+            <ul>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/users">Users</Link>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Route exact path="/" component={ Home } />
+          <Route path="/about" component={ About } />
+          <Route path="/users" component={ Users } />
+        </main>
       </BrowserRouter>
     );
   }

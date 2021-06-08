55ac42aea8f4f8f3b11d79c59984c99516d39be0
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-06-08 12:16:46 -0300

    Add fixation-exercise 12

--- 2.Front-end-Development/exercises/Unit_13/Day_2/class-exercises/comprehension-exercises-router-master/src/App.js
+++ 2.Front-end-Development/exercises/Unit_13/Day_2/class-exercises/comprehension-exercises-router-master/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import './App.css'
 import Home from './components/Home';
 import About from './components/About';
-import Users from './components/Users'
+import Users from './components/Users';
+import StrictAccess from './StrictAccess'
 
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
               <Link to="/">Home</Link>
               <Link to="/about">About</Link>
               <Link to="/users">Users</Link>
+              <Link to="/strict-access">Strict Acess</Link>
             </ul>
           </nav>
         </header>

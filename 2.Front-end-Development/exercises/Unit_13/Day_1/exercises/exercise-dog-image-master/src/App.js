1e4c3b0e967015baa7dece95cacbd613945047c8
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-06-07 22:18:32 -0300

    Add exercise-dog-image-master

--- 2.Front-end-Development/exercises/Unit_13/Day_1/exercises/exercise-dog-image-master/src/App.js
+++ 2.Front-end-Development/exercises/Unit_13/Day_1/exercises/exercise-dog-image-master/src/App.js
@@ -1,30 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import RenderDog from './components/RenderDog';
+import LoadingInfo from './components/LoadingInfo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.requestDog = this.requestDog.bind(this);
+
+    this.state = {
+      currentDog: '',
+      dogList: [],
+      isLoading: true,
+    }
+  }
+
+  async requestDog() {
+    this.setState(
+      { isLoading: true, },
+      async () => {
+        await fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+          this.setState((previusState) => ({
+            currentDog: data.message,
+            dogList: [...previusState.dogList, data.message],
+            isLoading: false,
+          }))
+        })
+      }
+    )
+    localStorage.setItem('DogList', this.state.dogList)
+  }
+
+  componentDidMount() {
+    this.requestDog()
+  }
+
+  shouldComponentUpdate(_nextProps, nextState) {
+    const { currentDog } = nextState
+
+    if (currentDog) {
+      let breeds = currentDog.split('s/')[1]
+      breeds = breeds.split('/')[0]
+      window.alert(`A raça desse cachorro é ${breeds}`)
+    }
+
+    if (currentDog.includes('terrier')) {
+      window.alert('Ops, essa raça não pode ser renderizada! Atualize a página.')
+      return false
+    } else {
+      return true
+    }
+  }
+
+  render() {
+    const { currentDog, isLoading } = this.state;
+
+    return (
+      <>
+        { isLoading ? <LoadingInfo /> : <RenderDog requestDog={ this.requestDog } currentDog={ currentDog } /> }
+      </>
+    )
+  }
 }
 
 export default App;

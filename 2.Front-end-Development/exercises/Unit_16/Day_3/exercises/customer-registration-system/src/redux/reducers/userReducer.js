369bfe0a964778cc41885b8081d510c6fde85692
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-07-05 10:07:03 -0300

    Add redux initial config

---
+++ 2.Front-end-Development/exercises/Unit_16/Day_3/exercises/customer-registration-system/src/redux/reducers/userReducer.js
@@ -0,0 +1,14 @@
+const INITIAL_STATE = {
+  users: [
+    {
+      nickName: 'SamuelMelo',
+      password: '123456',
+    },
+  ]
+}
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  return state;
+}
+
+export default userReducer;

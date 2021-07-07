e9a494de31f9fe74709a355ad40412fd36329ac9
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-07-07 10:29:03 -0300

    Add App.js

--- 2.Front-end-Development/exercises/Unit_16/Day_4/class-exercises/doguinhos-app/src/App.js
+++ 2.Front-end-Development/exercises/Unit_16/Day_4/class-exercises/doguinhos-app/src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchDog } from './store';
 
-function App() {
+function App({ isFetching, src, fetchDog }) {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    isFetching
+      ? <p>Loading</p>
+      : (
+        <div style={ { width: 500 } }>
+          <button
+            style={ { display: 'block' } }
+            onClick={fetchDog}
+            type="button"
+          >
+            Novo Doguinho
+          </button>
+          <img style={ { width: '100%' } } src={ src } alt="dog" />
+        </div>
+      )
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  src: state.imagePath,
+  isFetching: state.isFetching
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchDog: () => dispatch(fetchDog())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);

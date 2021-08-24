4110e8cdcf09a5bc88a14cb6782201bb5823677a
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-08-24 15:58:01 -0300

    Add filtering-GROUP-BY-results-with-HAVING.sql fixation exercise

---
+++ 3.Back-end-Development/exercises/Unit_21/Day_1/class-exercises/filtering-GROUP-BY-results-with-HAVING.sql
@@ -0,0 +1,27 @@
+/*
+1. Usando a query a seguir, exiba apenas as durações médias que estão entre 115.0 a 121.50. Além disso, dê um alias (apelido) à coluna gerada por AVG(length), de forma que deixe a query mais legível. Finalize ordenando os resultados de forma decrescente.
+
+	SELECT rating, AVG(length)
+	FROM sakila.film
+	GROUP BY rating;
+*/
+
+SELECT rating, AVG(length) AS media_duracao
+FROM sakila.film
+GROUP BY rating
+HAVING media_duracao BETWEEN '115.0' AND '121.50'
+ORDER BY media_duracao DESC;
+
+/*
+2. Usando a query a seguir, exiba apenas os valores de custo de substituição que estão acima de $3950.50. Dê um alias que faça sentido para SUM(replacement_cost), de forma que deixe a query mais legível. Finalize ordenando os resultados de forma crescente.
+
+SELECT rating, SUM(replacement_cost)
+FROM sakila.film
+GROUP by rating;
+*/
+
+SELECT rating, SUM(replacement_cost) AS total_replacement_cost
+FROM sakila.film
+GROUP by rating
+HAVING total_replacement_cost > 3950.50
+ORDER BY total_replacement_cost;

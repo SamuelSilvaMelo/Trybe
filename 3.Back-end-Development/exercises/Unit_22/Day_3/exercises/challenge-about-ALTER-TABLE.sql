fc3da93ff9c35498a7c7b063f5f91c38bf8d05a0
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-09-02 22:07:19 -0300

    Add Day 22.3 exercise challenge-about-ALTER-TABLE

---
+++ 3.Back-end-Development/exercises/Unit_22/Day_3/exercises/challenge-about-ALTER-TABLE.sql
@@ -0,0 +1,12 @@
+-- Desafios sobre ALTER TABLE
+-- 1. Escreva uma query SQL para alterar na tabela localtions o nome da coluna street_address para address, mantendo o mesmo tipo e tamanho de dados.
+
+ALTER TABLE hr.locations CHANGE COLUMN STREET_ADDRESS ADDRESS VARCHAR(40) DEFAULT NULL;
+
+-- 2. Escreva uma query SQL para alterar o nome da coluna region_name para region, mantendo o mesmo tipo e tamanho de dados.
+
+ALTER TABLE hr.regions CHANGE COLUMN REGION_NAME REGION VARCHAR(25) DEFAULT NULL;
+
+-- 3. Escreva uma query SQL para alterar o nome da coluna country_name para country, mantendo o mesmo tipo e tamanho de dados.
+
+ALTER TABLE hr.countries CHANGE COLUMN COUNTRY_NAME COUNTRY VARCHAR(40) DEFAULT NULL;

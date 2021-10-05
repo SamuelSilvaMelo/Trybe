5c2a0bed3b0002a33cd352498bcf3f52e0c2f892
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-10-04 23:17:19 -0300

    Add exercise 3 Day_26.5

--- 3.Back-end-Development/exercises/Unit_26/Day_5/exercises/index.js
+++ 3.Back-end-Development/exercises/Unit_26/Day_5/exercises/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const registerUser = require('./routes/passwords');
 const btcPrice = require('./routes/btcPrice');
+const posts = require('./routes/posts');
 
 const app = express();
 
 app.use(express.json());
 app.use('/user', registerUser);
 app.use('/btc', btcPrice);
+app.use('/posts', posts);
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001')

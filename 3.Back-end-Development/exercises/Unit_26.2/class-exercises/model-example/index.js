f38b53150aaf898a2aa70df3337645e210ab285e
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-10-08 20:25:03 -0300

    Add create author fixation exercise

--- 3.Back-end-Development/exercises/Unit_26.2/class-exercises/model-example/index.js
+++ 3.Back-end-Development/exercises/Unit_26.2/class-exercises/model-example/index.js
@@ -3,6 +3,8 @@ const author = require('./models/author');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/authors', async (_req, res) => {
   const authors = await author.getAll();
 
@@ -17,6 +19,18 @@ app.get('/authors/:id', async (req, res) => {
   if (!authorData) return res.status(404).json({ message: 'Not found' });
 
   res.status(200).json(authorData);
+});
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!author.isValid(first_name, middle_name, last_name)) {
+    return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await author.create(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'Autor criado com sucesso' });
 });
 
 const PORT = process.env.PORT || 3000;

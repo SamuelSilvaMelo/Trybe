5abb2288120323c33d021c2c8816892af1938d60
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-10-26 22:43:15 -0300

    Add exercise 2

--- 3.Back-end-Development/exercises/Unit_27/Day_2/exercises/multer-exercises/index.js
+++ 3.Back-end-Development/exercises/Unit_27/Day_2/exercises/multer-exercises/index.js
@@ -3,13 +3,41 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const multer = require('multer');
+const fs = require('fs');
 
 const storage = multer.diskStorage({
   destination: (_req, _file, callback) => callback(null, 'uploads/'),
-  filename: (_req, file, callback) => callback(null, `${Date.now()}-${file.originalname}`),
+  filename: (_req, file, callback) => callback(null, file.originalname),
 });
 
-const upload = multer({ storage });
+/* https://github.com/expressjs/multer/issues/114 */
+
+const upload = multer({
+  storage,
+  fileFilter: (_req, file, callback) => {
+    if (!file.originalname.includes('.png')) {
+      const error = {
+        statusCode: 403,
+        message: 'Extension must be `png`'
+      };
+
+      return callback(error);
+    }
+
+    const files = fs.readdirSync(`${__dirname}/uploads`);
+
+    if (files.some((dirFiles) => dirFiles === file.originalname)) {
+      const error = {
+        statusCode: 409,
+        message: 'File already exists'
+      };
+
+      return callback(error);
+    }
+
+    callback(null, true);
+  }
+});
 
 const { PORT } = process.env;
 

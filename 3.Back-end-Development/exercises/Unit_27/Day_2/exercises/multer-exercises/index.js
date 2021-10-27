9651f3feb4e79cad480a75e772f89740064f173f
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-10-26 22:54:32 -0300

    Fix diskStorage filename config

--- 3.Back-end-Development/exercises/Unit_27/Day_2/exercises/multer-exercises/index.js
+++ 3.Back-end-Development/exercises/Unit_27/Day_2/exercises/multer-exercises/index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 
 const storage = multer.diskStorage({
   destination: (_req, _file, callback) => callback(null, 'uploads/'),
-  filename: (_req, file, callback) => callback(null, file.originalname),
+  filename: (_req, file, callback) => callback(null, `${Date.now()}-${file.originalname}`),
 });
 
 /*

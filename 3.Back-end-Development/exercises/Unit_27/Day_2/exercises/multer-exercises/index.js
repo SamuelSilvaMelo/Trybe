7abd41ef5594b621250dbf0ece440f0cb9133b1d
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-10-26 22:17:16 -0300

    Add exercise 1

--- 3.Back-end-Development/exercises/Unit_27/Day_2/exercises/multer-exercises/index.js
+++ 3.Back-end-Development/exercises/Unit_27/Day_2/exercises/multer-exercises/index.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (_req, _file, callback) => callback(null, 'uploads/'),
+  filename: (_req, file, callback) => callback(null, `${Date.now()}-${file.originalname}`),
+});
+
+const upload = multer({ storage });
 
 const { PORT } = process.env;
 
@@ -22,6 +30,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/ping', controllers.ping);
+
+app.post('/upload', upload.single('file'), controllers.upload);
 
 app.use(middlewares.error);
 

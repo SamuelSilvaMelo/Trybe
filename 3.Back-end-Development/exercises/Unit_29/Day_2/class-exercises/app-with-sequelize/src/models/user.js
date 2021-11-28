aea2080af830c73476aa29add43e3bc56cce9d75
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-11-28 10:56:58 -0300

    Update table name to snake-case

--- 3.Back-end-Development/exercises/Unit_29/Day_2/class-exercises/app-with-sequelize/src/models/user.js
+++ 3.Back-end-Development/exercises/Unit_29/Day_2/class-exercises/app-with-sequelize/src/models/user.js
@@ -1,9 +1,16 @@
 const User = (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
+  const User = sequelize.define(
+    "User",
+    {
     fullName: DataTypes.STRING,
     email: DataTypes.STRING,
     phone_num: DataTypes.STRING,
-  });
+    },
+    {
+      underscored: true,
+      tableName: 'Users'
+    }
+  );
 
   return User;
 }

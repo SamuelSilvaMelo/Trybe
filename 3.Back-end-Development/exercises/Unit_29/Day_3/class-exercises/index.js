5bd6e3674994467b0740be24d9492a1f93a7d9b8
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-11-28 15:48:03 -0300

    Add sequelize N:N relationship fixation-exercise

--- 3.Back-end-Development/exercises/Unit_29/Day_3/class-exercises/index.js
+++ 3.Back-end-Development/exercises/Unit_29/Day_3/class-exercises/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Address, Employee } = require('./models');
+const { Address, Employee, Book, User } = require('./models');
 
 const app = express();
 
@@ -41,6 +41,24 @@ app.get(
     };
   }
 );
+
+app.get('/usersbooks/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({
+      where: { userId: id },
+      include: [{ model: Book, as: 'books', through: { attributes: [] } }],
+    });
+
+    if (!user)
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  };
+});
 
 const PORT = process.env.PORT || 3000;
 

b648bef8edef0252e42a1c28ae461bcf59c510a6
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-12-04 22:37:55 -0300

    Sending messages from the backend to the frontend

--- 3.Back-end-Development/exercises/Unit_30/Day_3/class-exercises/index.js
+++ 3.Back-end-Development/exercises/Unit_30/Day_3/class-exercises/index.js
@@ -22,10 +22,13 @@ const io = require('socket.io')(http, options);
 */
 
 io.on('connection', (socket) => {
-  console.log(`Usuário conectado. ID: ${socket.id}`);
+  // console.log(`Usuário conectado. ID: ${socket.id}`);
+
+  socket.emit('ola', 'Que bom que você chegou aqui! Fica mais um cadin, vai ter bolo :)');
 
   socket.on('ping', () => {
-    console.log(`${socket.id} emitiu um ping!`)
+    console.log(`${socket.id} emitiu um ping!`);
+    io.emit('pong', `${socket.id} enviou um pong!`) // essa linha envia um aviso para o cliente que o ping chegou.
   });
 });
 

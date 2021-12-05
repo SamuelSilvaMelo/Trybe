c537500b22088d8d127a6420ca503d588e3cd9c3
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-12-05 18:25:15 -0300

    Add exercise 5, 6 and 7

--- 3.Back-end-Development/exercises/Unit_30/Day_3/exercises/src/api/socket/like.js
+++ 3.Back-end-Development/exercises/Unit_30/Day_3/exercises/src/api/socket/like.js
@@ -1,4 +1,5 @@
 let likes = 0;
+let stars = 0;
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
@@ -6,6 +7,12 @@ module.exports = (io) => {
       likes += 1;
 
       io.emit('updateLikes', likes);
+    });
+
+    socket.on('starPost', (updatedStars) => {
+      stars = updatedStars;
+      
+      socket.broadcast.emit('updateStars', stars);
     });
   });
 };

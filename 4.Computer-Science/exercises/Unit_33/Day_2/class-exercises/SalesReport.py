73e68d967d2cebefd366d8600d55659379f3df11
Samuel Silva Melo <samuel.silva.melo@hotmail.com>
2022-02-02 22:37:57 -0300

    Add fixation exercies 1

--- 4.Computer-Science/exercises/Unit_33/Day_2/class-exercises/SalesReport.py
+++ 4.Computer-Science/exercises/Unit_33/Day_2/class-exercises/SalesReport.py
@@ -1,9 +1,11 @@
 import json
+import csv
 
 
 class SalesReport():
-    def __init__(self, export_file):
-        self.export_file = export_file + '.json'
+    def __init__(self, export_file, extension):
+        self.extension = extension
+        self.export_file = export_file + extension
 
     def build(self):
         """ Aqui colocamos a lógica para a entidade 'se criar',
@@ -23,4 +25,19 @@ class SalesReport():
     def serialize(self):
         # Vamos gerar, aqui, o nosso relatório em formato JSON
         with open(self.export_file, 'w') as file:
-            json.dump(self.build(), file)
+            if self.extension == '.json':
+                json.dump(self.build(), file)
+            if self.extension == '.csv':
+                headers = ['Coluna 1', 'Coluna 2', 'Coluna 3']
+                writer = csv.DictWriter(file, fieldnames=headers)
+                writer.writeheader()
+                writer.writerows(self.build())
+
+
+# Classe, crie (em outras palavras, instancie!) uma nova entidade 'Relatório de vendas' para eu usar!
+
+meu_relatorio_de_vendas = SalesReport('meu_relatorio', '.csv')
+
+# Entidade 'meu_relatorio_de_vendas', que eu acabei de criar, imprima-se!
+
+meu_relatorio_de_vendas.serialize()

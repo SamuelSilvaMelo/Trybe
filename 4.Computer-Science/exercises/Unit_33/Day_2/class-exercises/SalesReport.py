212e44e522ca2ee79767414be911774aa5ff3a8e
Samuel Silva Melo <samuel.silva.melo@hotmail.com>
2022-02-02 22:46:13 -0300

    SalesReport.py refactored to use inheritance

--- 4.Computer-Science/exercises/Unit_33/Day_2/class-exercises/SalesReport.py
+++ 4.Computer-Science/exercises/Unit_33/Day_2/class-exercises/SalesReport.py
@@ -1,16 +1,12 @@
+from abc import ABC, abstractmethod
 import json
-import csv
 
 
-class SalesReport():
-    def __init__(self, export_file, extension):
-        self.extension = extension
-        self.export_file = export_file + extension
+class SalesReport(ABC):
+    def __init__(self, export_file):
+        self.export_file = export_file
 
     def build(self):
-        """ Aqui colocamos a lógica para a entidade 'se criar',
-        ou seja, criar um relatório imprimível. Por simplicidade,
-        vamos omitir essa lógica nos exemplos! """
         return [{
                 'Coluna 1': 'Dado 1',
                 'Coluna 2': 'Dado 2',
@@ -22,22 +18,12 @@ class SalesReport():
                 'Coluna 3': 'Dado C'
                 }]
 
+    @abstractmethod
     def serialize(self):
-        # Vamos gerar, aqui, o nosso relatório em formato JSON
-        with open(self.export_file, 'w') as file:
-            if self.extension == '.json':
-                json.dump(self.build(), file)
-            if self.extension == '.csv':
-                headers = ['Coluna 1', 'Coluna 2', 'Coluna 3']
-                writer = csv.DictWriter(file, fieldnames=headers)
-                writer.writeheader()
-                writer.writerows(self.build())
+        raise NotImplementedError
 
 
-# Classe, crie (em outras palavras, instancie!) uma nova entidade 'Relatório de vendas' para eu usar!
-
-meu_relatorio_de_vendas = SalesReport('meu_relatorio', '.csv')
-
-# Entidade 'meu_relatorio_de_vendas', que eu acabei de criar, imprima-se!
-
-meu_relatorio_de_vendas.serialize()
+class SalesReportJSON(SalesReport):
+    def serialize(self):
+        with open(self.export_file + '.json', 'w') as file:
+            json.dump(self.build(), file)

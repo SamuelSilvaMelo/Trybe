0f20d1382ab5744549bbdc3736f943e2b866e55b
Samuel Silva Melo <samuel.silva.melo@hotmail.com>
2022-02-21 22:36:01 -0300

    Adds explanation for countdown using recursion

--- 4.Computer-Science/exercises/Unit_35/Day_2/class-exercises/recursion_example.py
+++ 4.Computer-Science/exercises/Unit_35/Day_2/class-exercises/recursion_example.py
@@ -6,4 +6,18 @@ def countdown(n):  # nome da função e parâmetro
         countdown(n - 1)  # chamada de si mesma com um novo valor
 
 
-# countdown(5)
+countdown(5)
+
+# EXPLICAÇÃO DO ALGORITMO
+#
+# n = 5 -> não satisfaz a condição de parada / chama a função novamente: `countdown(5 - 1)`.  # primeira execução
+#
+# n = 4 -> não satisfaz a condição de parada / chama a função novamente: `countdown(4 - 1)`.  # segunda execução
+#
+# n = 3 -> não satisfaz a condição de parada / chama a função novamente: `countdown(3 - 1)`.  # terceira execução
+#
+# n = 2 -> não satisfaz a condição de parada / chama a função novamente: `countdown(2 - 1)`.  # quarta execução
+#
+# n = 1 -> não satisfaz a condição de parada / chama a função novamente: `countdown(1 - 1)`.  # quinta execução
+#
+# n = 0 -> satisfaz condição de parada / entra no if e printa "FIM!".                         # sexta execução

3c459ca05655f992a3ad962fe9cb79b83ed87882
Samuel Silva Melo <samuel.silva.melo@hotmail.com>
2022-03-05 17:10:25 -0300

    Add fixation exercies 1-b and 1-c

--- 4.Computer-Science/exercises/Unit_36/Day_3/class-exercises/fixation_exercises.py
+++ 4.Computer-Science/exercises/Unit_36/Day_3/class-exercises/fixation_exercises.py
@@ -22,3 +22,17 @@ class HashMap:
     def has(self, id_num):
         address = self.get_address(id_num)
         return self._buckets[address] is not None
+
+
+employees = [(14, "name1"), (23, "name2"), (10, "name3"), (9, "name4")]
+hash_map = HashMap()
+
+
+for id_num, name in employees:
+    employee = Employee(id_num, name)
+    hash_map.insert(employee)
+
+
+print(
+    hash_map.get_value(23)
+)

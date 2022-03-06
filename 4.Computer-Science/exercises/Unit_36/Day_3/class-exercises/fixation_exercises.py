b771c2d5c697bfe23098e514ad172d2a7cbbe996
Samuel Silva Melo <samuel.silva.melo@hotmail.com>
2022-03-05 22:29:29 -0300

    Add Separate Chaining solution

--- 4.Computer-Science/exercises/Unit_36/Day_3/class-exercises/fixation_exercises.py
+++ 4.Computer-Science/exercises/Unit_36/Day_3/class-exercises/fixation_exercises.py
@@ -6,18 +6,21 @@ class Employee:
 
 class HashMap:
     def __init__(self):
-        self._buckets = [None for i in range(10)]
+        self._buckets = [[] for i in range(10)]
 
     def get_address(self, id_num):
         return id_num % 10
 
     def insert(self, employee):
         address = self.get_address(employee.id_num)
-        self._buckets[address] = employee
+        self._buckets[address].append(employee)
 
     def get_value(self, id_num):
         address = self.get_address(id_num)
-        return self._buckets[address].name
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return item.name
+        return None
 
     def has(self, id_num):
         address = self.get_address(id_num)
@@ -25,7 +28,8 @@ class HashMap:
 
     def update_value(self, id_num, new_name):
         address = self.get_address(id_num)
-        self._buckets[address].name = new_name
+        for item in self._buckets[address]:
+            item.name = new_name
 
 
 employees = [(14, "name1"), (23, "name2"), (10, "name3"), (9, "name4")]
@@ -41,8 +45,8 @@ print(hash_map.get_value(23))
 
 # Exercise 2
 
-print(hash_map.get_value(30))
+print(hash_map.get_value(10))
 
-hash_map.update_value(30, "name30")
+hash_map.update_value(10, "name30")
 
-print(hash_map.get_value(30))
+print(hash_map.get_value(10))
